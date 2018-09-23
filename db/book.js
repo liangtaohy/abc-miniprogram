66f033767f4ef23c02e88788d78faa732eccd55a
Liang Tao <liangtaohy@163.com>
2018-09-23 22:41:26 +0800

    提交动物词典

--- db/book.js
+++ db/book.js
@@ -1,6 +1,6 @@
 let book = [
 	{id: 1, name: "ABC", cover: "", comment: "字母类"},
-	{id: 2, name: "Fruit", cover: "", comment: "水果类"},
+	{id: 2, name: "Fruit", cover: "http://imgsrc.baidu.com/baike/pic/item/8b82b9014a90f6033d0618cc3f12b31bb151ed83.jpg", comment: "水果类"},
 	{id: 3, name: "Vegetables", cover: "", comment: "蔬菜类"},
 	{id: 4, name: "Transportation", cover: "", comment: "交通工具类"},
 	{id: 5, name: "123", cover: "", comment: "数字类"},
